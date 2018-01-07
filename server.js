af630c638a626f223cffc666e2280bea60213c16
dsimmon1 <simmons.diana93@gmail.com>
2018-01-07 10:13:02 -0800

    added ejs, resume page, and more pictures

--- server.js
+++ server.js
@@ -3,14 +3,60 @@
 const nodemailer = require('nodemailer');
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require('path');
 
-var app = express();
+const app = express();
 
 var PORT = process.env.PORT || 8080;
 
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
+app.set('views', __dirname + '/views');
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, '/public')));
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/js", express.static(__dirname + "/js"));
+app.use("/documents", express.static(__dirname + "/documents"));
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get("/", function(req, res) {
+    res.render("pages/index");
+    });
+
+app.get("/biography", function(req, res) {
+    res.render("pages/biography");
+    });
+
+app.get("/resume", function(req, res) {
+    res.render("pages/resume");
+    });
+
+app.get("/contact", function(req, res) {
+    res.render("pages/contact");
+    });
+
+app.get("/gallery", function(req, res) {
+    res.render("pages/gallery");
+    });
+
+app.post("/contact", function(req, res) {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `
+  console.log(output);
+
+      let transporter = nodemailer.createTransport({
         service: "Gmail",
         auth: {
             user: "simmons.diana93@gmail.com",
@@ -21,10 +67,11 @@ var PORT = process.env.PORT || 8080;
     // setup email data with unicode symbols
     let mailOptions = {
         from: '"Diana Simmons" <simmons.diana93@gmail.com>', // sender address
-        to: 'simmons.diana93@gmail.com', // list of receivers
-        subject: 'Hello ✔', // Subject line
+        // to: 'iamariellesimonee@gmail.com',
+        // to: 'simmons.diana93@gmail.com',
+        subject: 'New Contact from Website', // Subject line
         text: 'Hello world?', // plain text body
-        html: '<b>Hello world?</b>' // html body
+        html: output // html body
     };
 
     // send mail with defined transport object
@@ -39,9 +86,7 @@ var PORT = process.env.PORT || 8080;
         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
     });
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+    });
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
